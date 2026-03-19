2b91634bae6e79ec1ba5979da200f818a1b6f8d3
poby <laancer4@gmail.com>
2026-03-19 16:42:28 +0900

    init

---
+++ src/main/kotlin/com/calaf/overloadmanager/OverloadManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.calaf.overloadmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OverloadManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<OverloadManagerApplication>(*args)
+}
---
+++ src/test/kotlin/com/calaf/overloadmanager/OverloadManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.calaf.overloadmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OverloadManagerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
